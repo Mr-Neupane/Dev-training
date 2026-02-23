2c61454089303714a0179bdca04f7e93328eadbe
Mr-Neupane <nischal.prathamit@gmail.com>
2026-02-23 17:22:16 +0545

    feat: user create and edit

---
+++ Dotnet-Mvc/Controllers/UserController.cs
@@ -0,0 +1,90 @@
+﻿using Dotnet_Mvc.Dtos;
+using Dotnet_Mvc.Models;
+using Dotnet_Mvc.Services.Interface;
+using Dotnet_Mvc.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet_Mvc.Controllers;
+
+public class UserController : Controller
+{
+    private static List<UserModel> _list = new List<UserModel>();
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(AddUserVm vm)
+    {
+        var existing = _list.FirstOrDefault(x => x.UserName == vm.Name.Trim());
+        if (existing == null)
+        {
+            var dto = new NewUseDto
+            {
+                UserName = vm.Name,
+                Email = vm.Email,
+                Address = vm.Address,
+                Password = vm.Pass
+            };
+            var data = await _userService.AddUserAsync(dto);
+            _list.Add(data);
+            return RedirectToAction("UserReport");
+        }
+        else
+        {
+            throw new Exception("User with same name already exists");
+        }
+    }
+
+    public IActionResult UserReport()
+    {
+        var report = _list;
+        return View(report);
+    }
+
+    public IActionResult Edit(Guid id)
+    {
+        var existing = _list.FirstOrDefault(x => x.Id == id);
+        if (existing == null)
+        {
+            throw new Exception("User not found");
+        }
+        else
+        {
+            var res = new EditUserVm
+            {
+                UserId = existing.Id,
+                UserName = existing.UserName,
+                Email = existing.Email,
+                Address = existing.Address,
+            };
+            return View(res);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(EditUserVm vm)
+    {
+        var details = _list.Find(x => x.Id == vm.UserId);
+
+        if (details == null)
+        {
+            throw new Exception("User not found");
+        }
+        else
+        {
+            _userService.EditUserAsync(vm);
+        }
+
+        return RedirectToAction("UserReport");
+    }
+}
---
+++ Dotnet-Mvc/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using Dotnet_Mvc.Dtos;
+using Dotnet_Mvc.Models;
+using Dotnet_Mvc.Services.Interface;
+using Dotnet_Mvc.ViewModel;
+
+namespace Dotnet_Mvc.Services;
+
+public class UserService : IUserService
+{
+    private static List<UserModel> _list = new List<UserModel>();
+
+    public async Task<UserModel> AddUserAsync(NewUseDto dto)
+    {
+        var id = Guid.NewGuid();
+        var model = new UserModel
+        {
+            Id = id,
+            UserName = dto.UserName,
+            Email = dto.Email,
+            Address = dto.Address,
+            Password = dto.Password,
+        };
+        _list.Add(model);
+        return model;
+    }
+
+    public void EditUserAsync(EditUserVm vm)
+    {
+        var details = _list.FirstOrDefault(x => x.Id == vm.UserId);
+        if (details == null)
+        {
+            throw new Exception("User not found");
+        }
+        else
+        {
+            details.UserName = vm.UserName;
+            details.Email = vm.Email;
+            details.Address = vm.Address;
+        }
+    }
+}
