05248b6207de7948bd64b60fe36b01446eb1f5d5
Mr-Neupane <nischal.prathamit@gmail.com>
2026-03-01 11:36:23 +0545

    add branch table

---
+++ Dotnet-Mvc/Entities/Branch.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dotnet_Mvc.Entities
+{
+    [Table("branch", Schema = "general_setup")]
+    public class Branch : BaseEntity
+    {
+        public string BranchName { get; set; }
+        public string? BranchAddress { get; set; }
+        public string? BranchPhone { get; set; }
+        public int RecById { get; set; }
+        public virtual User RecBy { get; set; }
+    }
+}
